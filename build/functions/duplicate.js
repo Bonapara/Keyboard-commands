8ac6d39896e6b37365a30b7d58c005999be05276
Thomas des Francs <tdesfrancs@gmail.com>
2024-12-19 17:59:28 +0100

    new engine

--- build/functions/duplicate.js
+++
@@ -1,9 +0,0 @@
-export function duplicate() {
-    const selection = figma.currentPage.selection;
-    if (selection.length === 0) {
-        throw new Error('No items selected');
-    }
-    const duplicates = selection.map(node => node.clone());
-    figma.currentPage.selection = duplicates;
-    figma.notify('Items duplicated');
-}
--- build/functions/group.js
+++
@@ -1,9 +0,0 @@
-export function group() {
-    const selection = figma.currentPage.selection;
-    if (selection.length < 2) {
-        throw new Error('Select at least 2 items to group');
-    }
-    const groupNode = figma.group(selection, figma.currentPage);
-    figma.currentPage.selection = [groupNode];
-    figma.notify('Items grouped');
-}
